4f6678920f648739a1cf5d9e812d91080c03289e
Erjok17 <mrerjok@gmail.com>
2025-05-28 10:32:34 +0300

    heaven

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,6 +9,7 @@ import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 import { updateInvoice, State } from '@/app/lib/actions';
 import { useActionState } from 'react';
+
  
 export default function EditInvoiceForm({
   invoice,
--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -9,6 +9,7 @@ import {
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
 import { useActionState } from 'react';
+
 import { authenticate } from '@/app/lib/actions';
 import { useSearchParams } from 'next/navigation';
 
